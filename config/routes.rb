65130df3ab706dfa777d91d4bc566f9aa70f44d1
Junior Duda <duda.ifal@gmail.com>
2020-10-07 19:50:43 -0300

    feature/6 - update edit html for project - update index html for project - update new html for project - update application routes
    Signed-off-by: Junior Duda <duda.ifal@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #
   #
-  get '/projects', to: 'projects#index'
-  get '/projects/new', to: 'projects#new'
-  get '/projects/:id/edit', to: 'projects#edit'
-  post '/projects/:id', to: 'projects#update'
-  post '/projects', to: 'projects#create'
+  resources :projects
 end
