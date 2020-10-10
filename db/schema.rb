c395c21618d85b83e951bbd476e9d69b86cd6c29
Anderson Fernandes <fernandesanderson14@gmail.com>
2020-10-10 12:05:53 -0300

    feature/4 Add complaint table migration
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_10_005334) do
+ActiveRecord::Schema.define(version: 2020_10_10_145333) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "complaints", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "description", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_complaints_on_user_id"
+  end
 
   create_table "projects", force: :cascade do |t|
     t.text "title"
@@ -34,4 +43,5 @@ ActiveRecord::Schema.define(version: 2020_10_10_005334) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "complaints", "users"
 end
