831fa1ae7d6ce483797daa85241d78d390dacb8f
Anderson Fernandes <fernandesanderson14@gmail.com>
2020-10-10 12:21:21 -0300

    feature/4 Add complaint and user model specs
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

--- app/models/complaint.rb
+++ app/models/complaint.rb
@@ -1,3 +1,5 @@
 class Complaint < ApplicationRecord
   belongs_to :user, inverse_of: :complaints
+
+  validates :title, :description, presence: true
 end
--- spec/models/complaint_spec.rb
+++ spec/models/complaint_spec.rb
@@ -1,4 +1,17 @@
 require 'rails_helper'
 
-describe Complaint, type: :model do
+RSpec.describe Complaint, type: :model do
+  context 'relations' do
+    it { should belong_to(:user) }
+  end
+
+  context 'validations' do
+    describe '#title' do
+      it { should validate_presence_of(:title) }
+    end
+
+    describe '#description' do
+      it { should validate_presence_of(:description) }
+    end
+  end
 end
