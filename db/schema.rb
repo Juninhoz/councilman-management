d56b4c38a0977da56e815b251cea8469ac957fab
Elisangela <elisangela00silva@gmail.com>
2020-10-31 10:35:33 -0300

    Create migration
    Signed-off-by: Elisangela <elisangela00silva@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_10_145333) do
+ActiveRecord::Schema.define(version: 2020_10_31_132513) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "comment", null: false
+    t.bigint "user_id", null: false
+    t.bigint "project_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["project_id"], name: "index_comments_on_project_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "complaints", force: :cascade do |t|
     t.string "title", null: false
@@ -43,5 +53,7 @@ ActiveRecord::Schema.define(version: 2020_10_10_145333) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "projects"
+  add_foreign_key "comments", "users"
   add_foreign_key "complaints", "users"
 end
